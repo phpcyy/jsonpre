558e59d196230fe3a60c5a55e5315bac79c3c3a5
phpcyy <phpcyy@163.com>
2017-12-21 14:17:10 +0800

    add jsonpre.php.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jsonpre
 make json look pretty.
+# installation
+copy `jsonpre.php` into your bin path, and give it execute permission with `chmod +x`.
---
+++ jsonpre.php
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+<?php
+if(ftell(STDIN) === 0){
+    $input = stream_get_contents(STDIN);
+    $input = json_decode($input);
+    if (json_last_error() > 0){
+        echo "input is not a valid json. ";
+    } else {
+        echo json_encode($input,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    }
+}else{
+    $argv = array_slice($argv, 1);
+    if (empty($argv)) {
+        echo 
+"Usage: 
+    jsonpre [files...]";
+    }
+    foreach($argv as $filename){
+        if (!file_exists($filename)) {
+            echo $filename, " doesn't exist";
+        }else {
+            $input = file_get_contents($filename);
+            $input = json_decode($input);
+            if (json_last_error() > 0){
+                echo "file {$filename} is not a valid json. ";
+            } else {
+                echo json_encode($input,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+            }
+        }
+    }
+}
